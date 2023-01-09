7923602ec506a078a59b3ccead0c4ac31e6ad280
Durgajanardhana Subrahmanyam Gavara <62685908+Durgajanardhana@users.noreply.github.com>
2023-01-09 10:55:39 +0530

    Update streamlit_app.py
    Added fruit Smoothie heading

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,8 @@ streamlit.text('🥗    Kale, spinach and Rocket smoothie')
 streamlit.text('🐔    Hard Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
 
 
