49ecf558bf51f91596e2c72ce16891b002e0db9f
Durgajanardhana Subrahmanyam Gavara <62685908+Durgajanardhana@users.noreply.github.com>
2023-01-09 11:05:03 +0530

    Update streamlit_app.py
    Picking made easy by name

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,9 @@ streamlit.text('🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+# This Allows us to use names instead of numbers
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
