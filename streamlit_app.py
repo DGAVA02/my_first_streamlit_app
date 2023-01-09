7431247bc51b408a7d0493044475a418815b4506
Durgajanardhana Subrahmanyam Gavara <62685908+Durgajanardhana@users.noreply.github.com>
2023-01-09 10:51:07 +0530

    Update streamlit_app.py
    Added Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents new healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 and Blue Berry Oatmeal')
+streamlit.text('Kale, spinach and Rocket smoothie')
+streamlit.text('Hard Boiled Free-Range Egg')
+
+
