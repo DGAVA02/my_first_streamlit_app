9d845664978e87e0bed01f041ca3cd06f1ea2700
Durgajanardhana Subrahmanyam Gavara <62685908+Durgajanardhana@users.noreply.github.com>
2023-01-11 11:08:55 +0530

    Update streamlit_app.py
    adding header and modifying the fruityvice api data

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-
-streamlit.text(fruityvice_response)
+streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response.json())
 
 
 
