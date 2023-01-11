f092dc61160f64bb5035190c3fcc52680566706d
Durgajanardhana Subrahmanyam Gavara <62685908+Durgajanardhana@users.noreply.github.com>
2023-01-11 11:14:51 +0530

    Update streamlit_app.py
    changed the fruityvice api url

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
@@ -29,7 +29,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
 
 #take the json version of data and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
